885d492828dc96a835029d4838058629d3ac15b6
LePhroger <132900761+LePhroger@users.noreply.github.com>
2024-05-21 11:10:27 +0000

    player introduction started

--- src/adventure.py
+++ src/adventure.py
@@ -1,1 +1,9 @@
-print('Hello Adventurer, welcome to the Terminal Adventure')
+print('Hello Adventurer, welcome to the Terminal Adventure ')
+
+# player set up
+print('What is your name adventurer! ')
+p_name = input('')
+
+print('Okay ', p_name, " Would you like an introduction? ")
+choise = input('')
+
