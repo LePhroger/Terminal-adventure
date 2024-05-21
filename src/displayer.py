3bde3fda021f46978588de9e25c5dba20bfdbfed
Szymon <81180880+ender-titan1@users.noreply.github.com>
2024-05-21 09:50:41 +0000

    Displayer ready

--- src/displayer.py
+++ src/displayer.py
@@ -5,6 +5,12 @@ class Displayer:
     def __init__(self, witdh):
         self.witdh = witdh
 
+    def safe_get(self, ls, idx, default=None):
+        try:
+            return ls[idx]
+        except IndexError:
+            return default
+
     def screen(self, people, resources):
         height = max(len(people), len(resources))
         string = ""
@@ -12,23 +18,26 @@ class Displayer:
         os.system("clear")
 
         for i in range(height):
-            person = people.get(i)
-            resource = resources.get(i)
+            person = self.safe_get(people, i)
+            resource = self.safe_get(resources, i)
+            space = self.witdh
+            string = ""
 
-            person_str = str(person)
-            resource_str = str(resource)
-            space = self.witdh - Person.LENGTH - Resource.LENGTH
+            if person != None:
+                space -= Person.LENGTH
+                string += str(person)
 
-            string += person_str
-            string += ' ' * space
-            string += resource_str
-            string += "\n"
+            if resource != None:
+                space -= Resource.LENGTH
+                string += ' ' * space
+                string += str(resource)
 
-        print(string)
+            print(string)
+
         print("-" * self.witdh)
 
 d = Displayer(80)
-p = Person("Jack", 4, Status.NONE, 0)
+p = Person("Jack", 4, Status.FREEZING, 4)
 r2 = Resource("Heat", 10, "yellow")
 r = Resource("Wood", 10, "green")
 
