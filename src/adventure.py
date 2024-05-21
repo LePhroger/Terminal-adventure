fae568b2d5423f1ebbee57b6558a9d369728b83a
LePhroger <132900761+LePhroger@users.noreply.github.com>
2024-05-21 11:34:41 +0000

    created introduction scene

--- src/adventure.py
+++ src/adventure.py
@@ -7,3 +7,11 @@ p_name = input('')
 print('Okay ', p_name, " Would you like an introduction? ")
 choise = input('')
 
+y = ['y', 'yes']
+n = ['n', 'no']
+
+if choise in y:
+    print('INSERT LORE HERE')
+else:
+    print('''No problem ',p_name,' If anything 
+type Help to recive instructions''')
